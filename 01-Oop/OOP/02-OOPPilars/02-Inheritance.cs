b7590088620a4bb09f8bc0d348a4ea61984054a4
Matheus Viana <matheus_o_viana@hotmail.com>
2022-09-06 22:51:58 -0300

    added concepts to Inheritance

--- 01-Oop/OOP/02-OOPPilars/02-Inheritance.cs
+++ 01-Oop/OOP/02-OOPPilars/02-Inheritance.cs
@@ -1,7 +1,26 @@
-namespace OOPPilars
+namespace OOP
 {
-  public class Test
+  // Always when you Inherite a class you're assuming the base class is the same as the class being inherited
+  // always the inherited class transmits its states and behaviour to the child class
+  public class Employee : Person
   {
+    public DateTime AdmissionDate { get; set; }
+    public string Register { get; set; }
+  }
 
+  public class Process
+  {
+    public void Run()
+    {
+      var employee = new Employee
+      {
+        Name = "Mark",
+        BirthDate = Convert.ToDateTime("1999/10/27"),
+        AdmissionDate = DateTime.UtcNow,
+        Register = "0003"
+      };
+
+      employee.AgeCalculator();
+    }
   }
 }
