272f3cc491a45226272e4d4852bbb0e0346f409b
Matheus Viana <matheus_o_viana@hotmail.com>
2022-09-19 22:17:43 -0300

    Created ClienteService for solution of violation of SR Principle

---
+++ SOLID/1-SRP/SRP.Solucao/ClienteService.cs
@@ -0,0 +1,18 @@
+﻿namespace SOLID.SRP.Solucao
+{
+    public class ClienteService
+    {
+        public string AdicionarCliente(Cliente cliente)
+        {
+            if (!cliente.Validar())
+                return "Dados inválidos";
+
+            var repo = new ClientRepository();
+            repo.AdicionarCliente(cliente);
+
+            EmailServices.Enviar("email@email.com", cliente.Email.Endereco, "Bem vindo", "Parabéns! Você foi cadastrado");
+
+            return "Cliente cadastrado com sucesso";
+        }
+    }
+}
