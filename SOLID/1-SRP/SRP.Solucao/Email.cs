0f40cba300cd323e2e92b36699ca6a284271bfba
Matheus Viana <matheus_o_viana@hotmail.com>
2022-09-19 22:18:22 -0300

    Created Emailfor solution of violation of SR Principle

---
+++ SOLID/1-SRP/SRP.Solucao/Email.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SOLID.SRP.Solucao
+{
+    public class Email
+    {
+        public string Endereco { get; set; }
+
+        public bool Validar()
+            => Endereco.Contains("@");
+    }
+}
